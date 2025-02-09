a7159de3e693f189a8665c0a38d2817c15c014ed
Sai Kiran <skiran.mekala@gmail.com>
2025-02-09 12:54:35 +0000

    Initial frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+// import AstronomyPicture from "./pages/AstronomyPicture";
+import MarsRover from "./pages/MarsRover";
+import AsteroidTracker from "./pages/AsteroidTracker";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          {/* <Route path="apod" element={<AstronomyPicture />} /> */}
+          <Route path="mars" element={<MarsRover />} />
+          <Route path="neo" element={<AsteroidTracker />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
