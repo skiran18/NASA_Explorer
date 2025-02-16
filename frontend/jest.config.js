5aa7dc1eb126e6c89976f89a4fc9d114e73a36c1
Sai Kiran <skiran.mekala@gmail.com>
2025-02-16 18:19:51 +0000

    configuration files

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,16 +1,14 @@
 /** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',  "^react-router-dom$": "<rootDir>/node_modules/react-router-dom",
-  },
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest'
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!(lucide-react|recharts)/)', "/node_modules/(?!(react-router-dom)/)",
-  ],
-  testMatch: ['**/__tests__/**/*.js', '**/*.test.jsx', '**/*.test.js'],
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+export const testEnvironment = 'jsdom';
+export const setupFilesAfterEnv = ['<rootDir>/jest.setup.js'];
+export const moduleNameMapper = {
+  '\\.(css|less|scss|sass)$': 'identity-obj-proxy', "^react-router-dom$": "<rootDir>/node_modules/react-router-dom",
 };
+export const transform = {
+  '^.+\\.(js|jsx)$': 'babel-jest'
+};
+export const transformIgnorePatterns = [
+  '/node_modules/(?!(lucide-react|recharts)/)', "/node_modules/(?!(react-router-dom)/)",
+];
+export const testMatch = ['**/__tests__/**/*.js', '**/*.test.jsx', '**/*.test.js'];
+export const moduleFileExtensions = ['js', 'jsx', 'json', 'node'];
